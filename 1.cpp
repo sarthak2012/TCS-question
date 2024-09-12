b5a119307e4ccebd5c5c1da71c59935ccf08b591
Sarthak Srivastava <Sarthaksrivastava5765@gmail.com>
2024-09-13 00:23:41 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,7 @@
 /*
 there are n people in a room and they all shake there hand with each other such that no same 2 person can shake hands together 
 find the total no of handshakes
-formula = n*(n+1)/2
+formula = n*(n-1)/2
 */
 #include <iostream> 
 using namespace std; 
@@ -15,7 +15,7 @@ if(no_of_people <2){
     cout<<"cannot have any handshake here";
 }
 else
-handshake=(no_of_people *(no_of_people +1))/2;
+handshake=(no_of_people *(no_of_people -1))/2;
 
 
     cout<< handshake ;
